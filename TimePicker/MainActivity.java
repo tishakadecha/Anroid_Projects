93fae64df8bf1025eb5eb2e25b16922b1342312b
Tisha Kadecha <72653803+tishakadecha@users.noreply.github.com>
2023-04-17 00:21:05 +0530

    Time Picker

---
+++ TimePicker/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.timepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+    TimePicker timePicker;
+    TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timePicker = findViewById(R.id.timepicker);
+        t1 = findViewById(R.id.t1);
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                t1.setText(i+":"+i1);
+            }
+        });
+    }
+}
